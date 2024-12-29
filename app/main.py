dbb92dcf59945c627312a8c136f22160541bb908
rva174 <rva174@yandex.ru>
2024-12-29 22:36:32 +0500

    init

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+
 from routers import task
 from routers import user
 
--- app/routers/task.py
+++ app/routers/task.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+
 router = APIRouter(prefix='/task', tags=['task'])
 
 @router.get('/')
